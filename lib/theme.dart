1d3c27ce35dcfb8498a6b606151846f24ab39d53
Desy <kbthnsklh6@gmail.com>
2024-05-04 08:16:36 +0700

    fix: menambahkan text theme di theme.dart

--- lib/theme.dart
+++ lib/theme.dart
@@ -13,66 +13,77 @@ TextStyle appBarTitleStyle = TextStyle(
   fontSize: 20,
   fontWeight: FontWeight.normal,
   color: onSecondary,
+  fontFamily: 'InknutAntiqua',
 );
 
 TextStyle textLabelStyle = TextStyle(
   fontSize: 12,
   fontWeight: FontWeight.w600,
   color: brownText,
+  fontFamily: 'InknutAntiqua',
 );
 
 TextStyle textInputStyle = TextStyle(
   fontSize: 14,
   fontWeight: FontWeight.bold,
   color: onSecondary,
+  fontFamily: 'InknutAntiqua',
 );
 
 TextStyle filledButtonLabelStyle = TextStyle(
   fontSize: 14,
   fontWeight: FontWeight.w500,
   color: onPrimary,
+  fontFamily: 'InknutAntiqua',
 );
 
 TextStyle outlinedButtonLabelStyle = TextStyle(
   fontSize: 14,
   fontWeight: FontWeight.w500,
   color: primary,
+  fontFamily: 'InknutAntiqua',
 );
 
 TextStyle titleStyle = TextStyle(
   fontSize: 20,
   fontWeight: FontWeight.w600,
   color: onSecondary,
+  fontFamily: 'InknutAntiqua',
 );
 
 TextStyle mainPriceStyle = TextStyle(
   fontSize: 14,
   fontWeight: FontWeight.bold,
   color: primary,
+  fontFamily: 'InknutAntiqua',
 );
 
 TextStyle secondaryPriceStyle = TextStyle(
   fontSize: 12,
   fontWeight: FontWeight.w500,
   color: primary,
+  fontFamily: 'InknutAntiqua',
 );
 
 TextStyle mainTextStyle = TextStyle(
   fontSize: 14,
   fontWeight: FontWeight.w600,
   color: onSecondary,
+  fontFamily: 'InknutAntiqua',
 );
 
 TextStyle secondaryTextStyle = TextStyle(
   fontSize: 12,
   fontWeight: FontWeight.w300,
   color: onSecondary,
+  fontFamily: 'InknutAntiqua',
 );
 
 TextStyle tertiaryTextStyle = TextStyle(
   fontSize: 12,
   fontWeight: FontWeight.w300,
   color: tertiary,
+  fontFamily: 'InknutAntiqua',
 );
 
 final appTheme = ThemeData.light().copyWith(
@@ -84,4 +95,13 @@ final appTheme = ThemeData.light().copyWith(
     onSecondary: onSecondary,
     onSurface: brownText,
   ),
+  textTheme: ThemeData.light().textTheme.copyWith(
+        headlineSmall: appBarTitleStyle,
+        titleLarge: titleStyle,
+        titleSmall: mainTextStyle,
+        labelLarge: textInputStyle,
+        labelMedium: textLabelStyle,
+        bodyMedium: secondaryTextStyle,
+        bodySmall: tertiaryTextStyle,
+      ),
 );

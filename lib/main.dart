7b33043eb4fd9c66d71e97e5b852bdfd7dd1050d
nandhach <nandhachrismawan@gmail.com>
2024-05-17 08:50:19 +0700

    nandha

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:free_market_pens_mobile/presentation/screen/buyer/home_page.dart';
+import 'package:free_market_pens_mobile/presentation/screen/buyer/keranjang_screen.dart';
+import 'package:free_market_pens_mobile/presentation/screen/buyer/toko_page.dart';
 import 'package:free_market_pens_mobile/presentation/screen/seller/alamat_screen.dart';
-import 'package:free_market_pens_mobile/presentation/widget/component/konfirmasi_icon_button.dart';
-import 'package:free_market_pens_mobile/presentation/widget/component/konfirmasi_text_column.dart';
-import 'package:free_market_pens_mobile/presentation/widget/component/text_column.dart';
 import 'package:free_market_pens_mobile/theme.dart';
 import 'package:free_market_pens_mobile/presentation/screen/buyer/riwayat_page.dart';
 import 'package:free_market_pens_mobile/presentation/widget/component/image_widget.dart';
